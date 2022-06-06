e91eaa60d97293cd0e99f49d847d8d48dcfabcfb
HakjaeLee97 <hakjae.lee.97@gmail.com>
2022-06-06 17:21:12 +0900

    solved 2133

--- 2133.py
+++ 2133.py
@@ -4,19 +4,20 @@
 
 N = int(input())
 
-cases =  [0] * (N+1)
-cases[0] = 0
+cases =  [0] * (N+1) # 인덱스와 세로줄의 길이를 맞추기 위함
+
+cases[0] = 1 # cases[0] =1로 하여 j가 들어가는 for문을 간결하게 함 
 if N >= 2:
     cases[2] = 3
 if N >= 4:
     cases[4] = 11
 
-for i in range(1,N+1):
-    if N % 2 == 1:
-        cases[i] = 0
-    elif N >= 5:
-        for i in range(4,N+1):
-            cases[i] += cases[i-2] + 3
-            cases[i] += cases[i-4] + 2
-
+    if N % 2 == 1:# N이 홀수면 항상 0
+        cases[N] = 0
+    elif N >= 4:
+        for j in range(6,N+1,2):
+            cases[j] += cases[j-2] * 3 
+            for k in range(4,j+1,2):
+                cases[j] += cases[j-k] * 2
+                
 print(cases[N])
