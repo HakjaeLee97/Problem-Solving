5557909ae635558fab10b9140b8504c82ae6ba83
Hakjae Lee <105211418+HakjaeLee97@users.noreply.github.com>
2022-08-23 09:16:25 +0900

    2220823 문제풀이

--- BaekJoon/1759 암호 만들기.java
+++ BaekJoon/1759 암호 만들기.java
@@ -8,6 +8,7 @@ import java.util.StringTokenizer;
 public class Main {
 	static int L,C;
 	static String[] comb, input;
+	static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine()," ");
@@ -19,21 +20,21 @@ public class Main {
 		comb = new String[L];
 		Arrays.sort(input);
 		combination(0,0,0);
-		
+		System.out.println(sb);
 
 	}
 	public static void combination(int depth, int start, int cnt) {
 		if(depth == L) {
-			if(cnt > 0 && cnt<=L-2) { //모음이 1개 이상,  자음이 2개 이상
-				for(String s : comb)System.out.print(s);
-				System.out.println();
+			if(cnt > 0 ) { //모음이 1개 이상,  자음이 2개 이상
+				for(String s : comb) sb.append(s);
+				sb.append("\n");
 			}
 			return;
 		}
 		for(int i = start; i<C; i++) {
 			comb[depth] = input[i];
 			if(input[i].equals("a") || input[i].equals("e")||input[i].equals("i")||input[i].equals("o")||input[i].equals("u") ) {
-				combination(depth+1,i+1,cnt+1);
+				if(cnt + 1<= L-2)combination(depth+1,i+1,cnt+1);
 			}else {
 				combination(depth+1,i+1,cnt);				
 			}
