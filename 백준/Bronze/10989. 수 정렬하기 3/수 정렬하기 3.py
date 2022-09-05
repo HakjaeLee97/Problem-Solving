cd383aeafb57e47fd1ce6d6362825e8812960476
Hakjae Lee <105211418+HakjaeLee97@users.noreply.github.com>
2022-09-05 18:53:17 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 9876 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,15 @@
+import sys
+
+N = int(sys.stdin.readline())
+MAX_NUM = 10000
+
+count = [0]*(MAX_NUM+1)
+
+for _ in range(N):
+    num = int(sys.stdin.readline())
+    count[num] += 1
+
+for i in range(1,MAX_NUM+1):
+    if count[i] != 0:
+        for _ in range(count[i]):
+            print(i)
