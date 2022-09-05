4406b765d4808cbb8b24b8106a0c2a9fc24ec98a
Hakjae Lee <105211418+HakjaeLee97@users.noreply.github.com>
2022-09-05 20:06:09 +0900

    [Gold IV] Title: 부분합, Time: 112 ms, Memory: 12556 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,19 +1,12 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		int N = Integer.parseInt(st.nextToken());
-		int S = Integer.parseInt(st.nextToken());
-		st = new StringTokenizer(br.readLine()," ");
+		int N = read();
+		int S = read();
 		int[] arr = new int[N+1];
 		
 		for(int i =0; i<N;i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = read();
 		}
 		
 
@@ -40,5 +33,19 @@ public class Main {
 			System.out.println(answer);
 		}
 	}
+	
+	private static int read() throws Exception {
+
+	    int c, n = System.in.read() & 15;
+
+	    while ((c = System.in.read()) > 32)
+	        n = (n << 3) + (n << 1) + (c & 15);
+
+	    if (c == 13) System.in.read();
+
+	    return n;
+
+	}
+
 
 }
