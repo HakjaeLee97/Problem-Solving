67e5311bfdaf106c1d64f53bf630359adf475b58
Hakjae Lee <105211418+HakjaeLee97@users.noreply.github.com>
2022-09-05 18:50:22 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%s\n", "\\    /\\ " );
+    printf("%s\n", " )  ( ') " );
+    printf("%s\n", "(  /  ) " );
+    printf("%s\n", " \\(__)|" );
+
+    return 0;
+}
