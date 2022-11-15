6a2d2dca31c6d0cd9195b08a3202d6a97cf993cc
Hakjae Lee <105211418+HakjaeLee97@users.noreply.github.com>
2022-11-15 11:54:36 +0900

    [Silver V] Title: 올림픽, Time: 108 ms, Memory: 12300 KB -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static class Nation implements Comparable<Nation>{
+		int Gold;
+		int Silver;
+		int Bronze;
+		int Index;
+		public Nation(int gold, int silver, int bronze, int index) {
+			super();
+			Gold = gold;
+			Silver = silver;
+			Bronze = bronze;
+			Index = index;
+		}
+		@Override
+		public int compareTo(Nation o) {
+			int r = -(this.Gold - o.Gold);
+			if(r == 0) {
+				r = -(this.Silver - o.Silver);
+				if(r == 0) {
+					r =-(this.Bronze - o.Bronze);
+				}
+			}
+			return r;
+		}
+		
+	}
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		PriorityQueue<Nation> q = new PriorityQueue<>();
+		int G,S,B,I;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			I = Integer.parseInt(st.nextToken());
+			G = Integer.parseInt(st.nextToken());
+			S = Integer.parseInt(st.nextToken());
+			B = Integer.parseInt(st.nextToken());
+			q.add(new Nation(G,S,B,I));
+		}
+		int rank = 0;
+		int cnt = 0;
+		G = -1;
+		S = -1;
+		B = -1;
+		for (int i = 0; i < N; i++) {
+			Nation n = q.poll();
+			if(!(n.Gold == G && n.Silver == S && n.Bronze == B)) {
+				if(cnt != 0) {
+					rank += cnt;
+					cnt = 0;
+				}else {
+				}
+				rank++;
+			}else {
+				cnt++;
+			}
+			G = n.Gold;
+			S = n.Silver;
+			B = n.Bronze;
+			if(n.Index == K) break;
+		}
+		System.out.println(rank);
+		br.close();
+	}
+
+}
