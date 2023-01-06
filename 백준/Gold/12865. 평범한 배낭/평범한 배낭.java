1685e297e2a4a4070ea4f3dd719db5bc761df7d1
Hakjae Lee <105211418+HakjaeLee97@users.noreply.github.com>
2023-01-06 12:05:34 +0900

    [Gold V] Title: 평범한 배낭, Time: 116 ms, Memory: 12116 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -17,17 +17,15 @@ public class Main {
 			items[i][1] = Integer.parseInt(st.nextToken());
 		}
 		
-		int[][] bag = new int[N+1][K+1];
+		int[] bag = new int[K+1];
 		
-		for (int i = 1; i <= K; i++) { // 무게
-			for (int j = 1; j <= N; j++) { //물건
-				bag[j][i] = bag[j-1][i];
-				if(i >= items[j][0]) {
-					bag[j][i] = Math.max(bag[j-1][i], items[j][1] + bag[j-1][i-items[j][0]] );
-				} 
+		for (int i = 1; i <= N; i++) { 
+			for (int j = K; j >= items[i][0]; j--) {
+					bag[j] = Math.max(bag[j], items[i][1] + bag[j-items[i][0]] );
+				
 			}
 		}
-		System.out.println(bag[N][K]);
+		System.out.println(bag[K]);
 		br.close();
 	}
 
